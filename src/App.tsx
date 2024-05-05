a6c4a04308325f83df4566dabff5aab7925b9d05
Nicolò Poggi <pogginicolo@proton.me>
2024-05-05 12:50:24 +0200

    :sparkles: Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import AuthProvider from "@providers/AuthProvider";
+import UserProvider from "@providers/UserProvider";
+import Navbar from "@components/Navbar";
 import Routes from "./routes";
 
 function App() {
 	return (
 		<div className="container mt-5">
 			<AuthProvider>
-				<Routes />
+				<UserProvider>
+					<>
+						<Navbar />
+						<Routes />
+					</>
+				</UserProvider>
 			</AuthProvider>
 		</div>
 	);
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,53 @@
+import { Navbar as BootstrapNavbar, Container } from "react-bootstrap";
+import { useUser, UserContext } from "@providers/UserProvider";
+import {
+	useAuth,
+	AuthContext,
+	AuthAPICallParams,
+} from "@providers/AuthProvider";
+import { useEffect } from "react";
+
+const Navbar = () => {
+	const { user, setUser }: UserContext = useUser();
+	const { accessToken, authApiCall }: AuthContext = useAuth();
+
+	useEffect((): void => {
+		if (accessToken) {
+			const thenCallback = (response: any) => {
+				console.log("user:", response.data);
+				setUser(response.data);
+			};
+
+			const catchCallback = (error: any) => {
+				console.error(error);
+			};
+
+			const authApiCallParams: AuthAPICallParams = {
+				method: "GET",
+				thenCallback,
+				catchCallback,
+			};
+			authApiCall("/api/account/user/", authApiCallParams);
+		} else {
+			setUser(null);
+		}
+	}, [accessToken]);
+
+	return (
+		<BootstrapNavbar className="bg-body-tertiary">
+			<Container>
+				<BootstrapNavbar.Brand href="#home">
+					Two Guys One Hike
+				</BootstrapNavbar.Brand>
+				<BootstrapNavbar.Toggle />
+				<BootstrapNavbar.Collapse className="justify-content-end">
+					<BootstrapNavbar.Text>
+						Signed in as: {user ? user.username : "Guest"}
+					</BootstrapNavbar.Text>
+				</BootstrapNavbar.Collapse>
+			</Container>
+		</BootstrapNavbar>
+	);
+};
+
+export default Navbar;
